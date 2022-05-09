a2dbbce1d51eceea030cf00a5287cbe11bea95b1
yeeahG <yeji.sep.k@gmail.com>
2022-05-10 02:38:53 +0900

    [JS]add login

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+// const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
+function onLoginSubmit(event) {
     // console.dir(loginInput.value);
-    const username = loginInput.value;
+    // const username = loginInput.value;
+    // console.log(username);
+    // if (username === "") {
+    //     alert("Please write your name");
+    // } else if(username.length > 15) {
+    //     alert("Your name is too long.");
+    // }
     
-    if (username === "") {
-        alert("Please write your name");
-    } else if(username.length > 15) {
-        alert("Your name is too long.");
-    }
-    
+    event.preventDefault();
+    console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+// loginButton.addEventListener("click", onLoginSubmit);
+
+//submit기능
+loginForm.addEventListener("submit", onLoginSubmit);
