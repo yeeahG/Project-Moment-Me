f322c6fede10f31b37516f8a489b38364aed1fc6
yeeahG <yeji.sep.k@gmail.com>
2022-05-15 20:17:33 +0900

    [ALL]add icons, photos, codes

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const loginForm = document.querySelector("#login-form");
+// const loginForm = document.querySelector("#login-form");
+const loginForm = document.querySelector("#container");
 const loginInput = document.querySelector("#login-form input");
-// const loginButton = document.querySelector("#login-form button");
 const greeting = document.querySelector("#greeting");
+
+const home = document.querySelector("#content");
 
 const title = document.querySelector("#title");
 const titleText = document.querySelector('#out-text');
+
+const todoContent = document.querySelector('#todo');
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -19,41 +23,41 @@ function onLoginSubmit(event) {
     //     alert("Your name is too long.");
     // }
     
-    event.preventDefault();
+    //--------------------------
+    // event.preventDefault();
 
     console.log(loginInput.value);
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
-    console.log(username);
 
     loginForm.classList.add(HIDDEN_CLASSNAME);
 
     paintGreetings(username);
-
     title.classList.remove(HIDDEN_CLASSNAME);
 }
 
-// function paintGreetings(username) {
-//     greeting.innerText = `Hello ${username}`;
-//     greeting.classList.remove(HIDDEN_CLASSNAME);
-// }
+function paintGreetings(username) {
+    // greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
 // loginButton.addEventListener("click", onLoginSubmit);
 
 //submit기능
 //loginForm.addEventListener("submit", onLoginSubmit);
 
-// use localstorage
+//----------use localstorage----------
 const savedUsername = localStorage.getItem(USERNAME_KEY);
-console.log(savedUsername);
+//console.log(savedUsername);
 
 if (savedUsername === null) {
     //show the form
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    //show th greetings
-    // paintGreetings(savedUsername);
-    title.classList.remove(HIDDEN_CLASSNAME);
+    //show the greetings
+    home.classList.remove(HIDDEN_CLASSNAME);
+    // title.classList.remove(HIDDEN_CLASSNAME);
     titleText.innerText = `${savedUsername}`;
+    // todoContent.classList.remove(HIDDEN_CLASSNAME);
 }
